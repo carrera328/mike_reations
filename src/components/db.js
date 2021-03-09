992e805a8aecd98f9631ee7f1548c18a7681f061
carrera328 <carrera.328@gmail.com>
2021-03-09 12:33:16 -0500

    Adding db mock file for use

---
+++ src/components/db.js
@@ -0,0 +1,90 @@
+export const data = [
+    {
+        "id" : 0,
+        "name" : "Moonpies",
+        "price" : 15.00,
+        "description": "I\'ve been smoking for a long time why should I change? and they say the ecstacy fucks up your brain mane",
+        "picklistValues": [
+            {"name":"Cheese", "chosen": false, "id" : 0},
+            {"name":"Bacon", "chosen": false, "id" : 1},
+            {"name":"Lettuce", "chosen": false, "id" : 2},
+            {"name":"Tomato", "chosen": false, "id" : 3},
+            {"name":"Onions", "chosen": false, "id" : 4}
+        ]
+        
+    },
+    {   "id" : 1,
+        "name" : "Candy Apple Tart",
+        "price" : 50.00,
+        "description": "Cocaine is great except if you are Juice Wrld",
+        "picklistValues": [
+            {"name":"Cheese", "chosen": false, "id" : 0},
+            {"name":"Bacon", "chosen": false, "id" : 1},
+            {"name":"Lettuce", "chosen": false, "id" : 2},
+            {"name":"Tomato", "chosen": false, "id" : 3},
+            {"name":"Onions", "chosen": false, "id" : 4}
+        ]
+
+    },
+    {
+        "id" : 2,
+        "name" : "Georgia",
+        "price" : 160.00,
+        "description": "Ecsta-si, ecstacy-no",
+        "picklistValues": [
+            {"name":"Cheese", "chosen": false, "id" : 0},
+            {"name":"Bacon", "chosen": false, "id" : 1},
+            {"name":"Lettuce", "chosen": false, "id" : 2},
+            {"name":"Tomato", "chosen": false, "id" : 3},
+            {"name":"Onions", "chosen": false, "id" : 4}
+        ]
+        
+    },
+    {
+        "id" : 3,
+        "name" : "Suzie Q",
+        "price" : 150.00,
+        "description": "Ecsta-si, ecstacy-no",
+        "picklistValues": [
+            {"name":"Cheese", "chosen": false, "id" : 0},
+            {"name":"Bacon", "chosen": false, "id" : 1},
+            {"name":"Lettuce", "chosen": false, "id" : 2},
+            {"name":"Tomato", "chosen": false, "id" : 3},
+            {"name":"Onions", "chosen": false, "id" : 4}
+        ]
+        
+    },
+    {
+        "id" : 4,
+        "name" : "AK-47",
+        "price" : 867.,
+        "description": "Ecsta-si, ecstacy-no",
+        "picklistValues": [
+            {"name":"Cheese", "chosen": false, "id" : 0},
+            {"name":"Bacon", "chosen": false, "id" : 1},
+            {"name":"Lettuce", "chosen": false, "id" : 2},
+            {"name":"Tomato", "chosen": false, "id" : 3},
+            {"name":"Onions", "chosen": false, "id" : 4}
+        ]
+        
+    },
+    {
+        "id" : 5,
+        "name" : "Chicken Fingers",
+        "price" : 5.00,
+        "description": "Test stuff baby boi lmao",
+        "picklistValues": [
+            {"name":"Cheese", "chosen": false, "id" : 0},
+            {"name":"Bacon", "chosen": false, "id" : 1},
+            {"name":"Lettuce", "chosen": false, "id" : 2},
+            {"name":"Tomato", "chosen": false, "id" : 3},
+            {"name":"Onions", "chosen": false, "id" : 4},
+            {"name":"Cheese", "chosen": false, "id" : 5},
+            {"name":"Bacon", "chosen": false, "id" : 6},
+            {"name":"Lettuce", "chosen": false, "id" : 7},
+            {"name":"Tomato", "chosen": false, "id" : 8},
+            {"name":"Onions", "chosen": false, "id" : 9}
+        ]
+        
+    }
+];
