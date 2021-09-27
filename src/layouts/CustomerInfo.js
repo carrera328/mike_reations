7b5374ae96923453fbfaac8209a389d993196f9a
Sal Carrera <carrera.328@gmail.com>
2021-09-27 04:55:13 -0400

    empty commit

--- src/layouts/CustomerInfo.js
+++ src/layouts/CustomerInfo.js
@@ -2,7 +2,8 @@ import React, {Fragment, useState} from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import HeadNav from '../components/HeadNav';
-import CustomerForm from '../components/CustomerForm';
+import CustomerForm from '../components/CustomerForm';\
+
 
 function CustomerInfo() {
     return (
